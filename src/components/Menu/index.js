e3b983a6cd5669e23dd0b5a7134361561b280eb1
William Cutrim da Silva <49375676+WilliamCutrim@users.noreply.github.com>
2020-07-28 23:44:26 -0300

    Building project

---
+++ src/components/Menu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+
+function Menu(){
+    return (
+        <nav>
+            <img src={Logo} />
+        </nav>
+
+    );
+
+
+}
+
+export default Menu;
